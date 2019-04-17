ff3771e55f401dcfc035a6034389d6c7c2ba4cdc
Yusuke1998 <jhonnyjose1998@gmail.com>
2019-04-17 17:33:58 -0400

    Insertar categoria usando vue y axios funcional

--- resources/views/contenido/contenido.blade.php
+++ resources/views/contenido/contenido.blade.php
@@ -1,7 +1,7 @@
     @extends('principal')
     @section('contenido')
         <template v-if="menu==0">
-            <div class="container">
+            <div class="p-5 m-5">
                 <h1>Contenido del menu: @{{ menu }}</h1>
             </div>
         </template>
@@ -9,57 +9,57 @@
             <categoria></categoria>
         </template>
         <template v-if="menu==2">
-            <div class="container">
+            <div class="p-5 m-5">
                 <h1>Contenido del menu: @{{ menu }}</h1>
             </div>
         </template>
         <template v-if="menu==3">
-            <div class="container">
+            <div class="p-5 m-5">
                 <h1>Contenido del menu: @{{ menu }}</h1>
             </div>
         </template>
         <template v-if="menu==4">
-            <div class="container">
+            <div class="p-5 m-5">
                 <h1>Contenido del menu: @{{ menu }}</h1>
             </div>
         </template>
         <template v-if="menu==5">
-            <div class="container">
+            <div class="p-5 m-5">
                 <h1>Contenido del menu: @{{ menu }}</h1>
             </div>
         </template>
         <template v-if="menu==6">
-            <div class="container">
+            <div class="p-5 m-5">
                 <h1>Contenido del menu: @{{ menu }}</h1>
             </div>
         </template>
         <template v-if="menu==7">
-            <div class="container">
+            <div class="p-5 m-5">
                 <h1>Contenido del menu: @{{ menu }}</h1>
             </div>
         </template>
         <template v-if="menu==8">
-            <div class="container">
+            <div class="p-5 m-5">
                 <h1>Contenido del menu: @{{ menu }}</h1>
             </div>
         </template>
         <template v-if="menu==9">
-            <div class="container">
+            <div class="p-5 m-5">
                 <h1>Contenido del menu: @{{ menu }}</h1>
             </div>
         </template>
         <template v-if="menu==10">
-            <div class="container">
+            <div class="p-5 m-5">
                 <h1>Contenido del menu: @{{ menu }}</h1>
             </div>
         </template>
         <template v-if="menu==11">
-            <div class="container">
+            <div class="p-5 m-5">
                 <h1>Contenido del menu: @{{ menu }}</h1>
             </div>
         </template>
         <template v-if="menu==12">
-            <div class="container">
+            <div class="p-5 m-5">
                 <h1>Contenido del menu: @{{ menu }}</h1>
             </div>
         </template>
